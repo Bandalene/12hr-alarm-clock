e0cdb0cf420d147e50e1dd6b5e855dbef5591214
Bandalene <73263770+Bandalene@users.noreply.github.com>
2022-01-20 08:17:58 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,39 @@
+function newClock() {
+    setTimeout(function() {
+        currentDate = new Date();
+        hours = currentDate.getHours();
+        minutes = currentDate.getMinutes();
+        seconds = currentDate.getSeconds();
+        customDate = `${hours}:${minutes}:${seconds}`;
+        newClock();
+        checkTime();
+        document.getElementById('demo').innerHTML = customDate;
+        playAlarm();
+    }, 1000)
+}
+
+var a
+var c
+stop = 1
+function checkTime() {
+    a = document.getElementById('hoursSet').value;
+    c = document.getElementById("minutesSet").value;
+    checkAlarm();
+}
+function checkAlarm() {
+if (a == hours , c == minutes) {
+    stop = 0
+    playAlarm();
+    }
+}
+function playAlarm() {
+    if (stop == 0) {
+    document.getElementById("sound").play()
+    }
+}
+
+function stopAlarm() {
+    stop = 1
+    document.getElementById('hoursSet').value = null
+    document.getElementById('minutesSet').value = null
+}

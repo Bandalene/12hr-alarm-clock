993e115984bec5531b969631712b4143160e69c7
Bandalene <73263770+Bandalene@users.noreply.github.com>
2022-01-21 19:04:53 -0500

    fixed alarm
    alarm was not working properly in previous commit

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let a
+let c
+stop = 1
 function newClock() {
     setTimeout(function() {
         currentDate = new Date();
@@ -7,13 +10,15 @@ function newClock() {
         seconds = seconds >= 10 ? `${seconds}` : `0${seconds}`;
         minutes = minutes >= 10 ? `${minutes}` : `0${minutes}`;
         if (hours > 12 && hours - 12 < 10) {
-            customDate = `0${hours-12}:${minutes}:${seconds}p.m.`
+            hours = `${hours-12}`;
+            customDate = `0${hours}:${minutes}:${seconds}pm`
         } else if (hours > 12) {
-            customDate = `${hours-12}:${minutes}:${seconds}p.m.`
+            hours = `${hours-12}`;
+            customDate = `${hours}:${minutes}:${seconds}pm`
         } else if (hours <= 12 && hours - 12 < 10) {
-            customDate = `0${hours}:${minutes}:${seconds}a.m.`
+            customDate = `0${hours}:${minutes}:${seconds}am`
         } else if (hours <= 12) {
-            customDate = `${hours}:${minutes}:${seconds}a.m.`
+            customDate = `${hours}:${minutes}:${seconds}am`
         }
         newClock();
         checkTime();
@@ -21,31 +26,22 @@ function newClock() {
         playAlarm();
     }, 1000)
 }
-
-let a
-let c
-stop = 1
-
-
 function checkTime() {
     a = document.getElementById("hoursSet").value;
     c = document.getElementById("minutesSet").value;
     checkAlarm();
 }
-
 function checkAlarm() {
 if (a == hours && c == minutes) {
     stop = 0
     playAlarm();
     }
 }
-
 function playAlarm() {
     if (stop == 0) {
     document.getElementById("sound").play()
     }
 }
-
 function stopAlarm() {
     stop = 1
     document.getElementById('hoursSet').value = null
